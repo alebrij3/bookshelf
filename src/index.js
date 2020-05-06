7a81ed21575b04275b2cf34ba47730abc3b52937
Alex <ajf@fastmail.com>
2020-05-06 11:58:43 -0500

    create 2 more books

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const initialState = {
   "read": [],
   "wantToRead": []
 }
-const book = {
+/* const book = {
   "title": "De cero a uno",
   "author": "Peter Thiel",
   "format": "Audiobook",
@@ -13,4 +13,25 @@ const book = {
   "category": "Business",
   "tags": ['startups','entrepreneurship','business','technology'],
   "duration": "6h 13m",
+} */
+class Book {
+  constructor(title, author, format, startDate, finishDate, excerpt, category, tags, duration) {
+    this.title = title;
+    this.author = author;
+    this.format = format;
+    this.startDate = startDate;
+    this.finishDate = finishDate;
+    this.excerpt = excerpt;
+    this.category = category;
+    this.tags = tags;
+    this.duration = duration;
+  }
 }
+
+const book1 = new Book("De cero a uno","Peter Thiel","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"6h 13m")
+
+const book2 = new Book("De cero a uno","Peter Thiel","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"6h 13m")
+
+const book3 = new Book("De cero a uno","Peter Thiel","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"6h 13m")
+
+console.log(book1);
