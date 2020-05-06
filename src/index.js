0cc756a892ba8b5b9c95ec60f569b962eb7d5bb8
Alex <ajf@fastmail.com>
2020-05-06 10:29:30 -0500

    basic book object structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+  "nowReading": [],
+  "read": [],
+  "wantToRead": []
+}
+const book = {
+  "title": "De cero a uno",
+  "author": "Peter Thiel",
+  "format": "Audiobook",
+  "startDate": "21/04/2020",
+  "finishDate": "24/04/2020",
+  "excerpt": "Lorem Ipsum",
+  "category": "Business",
+  "tags": ['startups','entrepreneurship','business','technology'],
+  "duration": "6h 13m",
+}
