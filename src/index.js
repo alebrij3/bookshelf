0e1772ba10862677988f5cd61d3a4c9870b65978
Alex <ajf@fastmail.com>
2020-05-07 02:24:32 -0500

    delete commented code

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ const initialState = {
   "read": [],
   "wantToRead": []
 }
-/* const book = {
-  "title": "De cero a uno",
-  "author": "Peter Thiel",
-  "format": "Audiobook",
-  "startDate": "21/04/2020",
-  "finishDate": "24/04/2020",
-  "excerpt": "Lorem Ipsum",
-  "category": "Business",
-  "tags": ['startups','entrepreneurship','business','technology'],
-  "duration": "6h 13m",
-} */
 class Book {
   constructor(title, author, format, startDate, finishDate, excerpt, category, tags, duration) {
     this.title = title;
