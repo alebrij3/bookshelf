b0bae447590fcc2389cbcdba6d276283d2c42c75
Alex <ajf@fastmail.com>
2020-05-07 02:43:36 -0500

    move the bookk class to the book component for reference

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,3 +5,19 @@ class Book extends React.Component {
     return <h1>Hello! I'm the Book component.</h1>
   }
 }
+
+class Bookk {
+  constructor(title, author, format, startDate, finishDate, excerpt, category, tags, duration) {
+    this.title = title;
+    this.author = author;
+    this.format = format;
+    this.startDate = startDate;
+    this.finishDate = finishDate;
+    this.excerpt = excerpt;
+    this.category = category;
+    this.tags = tags;
+    this.duration = duration;
+  }
+}
+
+export default Book;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Book from './components/Book';
 
+ReactDOM.render(
+  <Book />, document.getElementById('root')
+);
 const initialState = {
   "nowReading": [],
   "read": [],
   "wantToRead": []
 }
-class Book {
-  constructor(title, author, format, startDate, finishDate, excerpt, category, tags, duration) {
-    this.title = title;
-    this.author = author;
-    this.format = format;
-    this.startDate = startDate;
-    this.finishDate = finishDate;
-    this.excerpt = excerpt;
-    this.category = category;
-    this.tags = tags;
-    this.duration = duration;
-  }
-}
 
-const book1 = new Book("De cero a uno","Peter Thiel","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"6h 13m")
 
-const book2 = new Book("Shoe Dog","Phil Knight","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"10h 25m")
+const book1 = new Bookk("De cero a uno","Peter Thiel","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"6h 13m")
 
-const book3 = new Book("Mariposa","Yusra Mardini","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Biography",['immigration','refugee','travel','swimming'],"380p")
+const book2 = new Bookk("Shoe Dog","Phil Knight","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Business",['startups','entrepreneurship','business','technology'],"10h 25m")
+
+const book3 = new Bookk("Mariposa","Yusra Mardini","Audiobook","21/04/2020","24/04/2020","Lorem Ipsum","Biography",['immigration','refugee','travel','swimming'],"380p")
 
 console.log(book1);
