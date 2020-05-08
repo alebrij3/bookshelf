3f08f5537a9eefe620a83e274a100c4236169080
Alex <ajf@fastmail.com>
2020-05-07 20:04:22 -0500

    remove commented code

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Shelf from './containers/Shelf';
 
-/* const shoeDog = {
-  "id": 1,
-  "title": "Shoe Dog",
-  "author": "Phil Knight",
-  "format": "Audiobook",
-  "dateStart": "21/04/2020",
-  "dateEnd": "24/04/2020",
-  "excerpt": "Lorem Ipsum",
-  "category": "Business",
-  "tags": ['startups','entrepreneurship','business','technology'],
-  "duration": "10h 25m"
-}; */
-
 const initialState = {
   "nowReading": [
     {
