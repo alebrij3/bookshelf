71b81dc6bc722e55d07a844327b998edc8353436
Alex <ajf@fastmail.com>
2020-05-12 13:41:39 -0500

    moved the header in index.html o the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Hero extends React.Component {
   render() {
     return(
-      <h1>Hi!</h1>
+      <>
+      <h1>Hi! This is my Bookshelf</h1>
+      <p>Here are the books I've read / want to read / I'm reading</p>
+      </>
     );
   }
 }
