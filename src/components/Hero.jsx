66e95b2a980d40f454aaa883c01e64b4daef25b5
Alex <ajf@fastmail.com>
2020-05-12 15:40:28 -0500

    Styling for hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import '../assets/styles/components/Hero.scss';
 
 class Hero extends React.Component {
   render() {
     return(
-      <>
-      <h1>Hi! This is my Bookshelf</h1>
-      <p>Here are the books I've read / want to read / I'm reading</p>
-      </>
+      <div id="hero">
+        <div className="text">
+          <p>Hi! This is my Bookshelf</p>
+          <p>Here are the books I've read / want to read / I'm reading</p>
+        </div>
+      </div>
     );
   }
 }
