562bed3ed19600b97e6e64af8f60ff3279bed2e1
Alex <ajf@fastmail.com>
2020-05-12 15:44:24 -0500

    ADD Shelf styles file and imported into the Shelf container

--- src/containers/Shelf.jsx
+++ src/containers/Shelf.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Book from '../components/Book';
-import AddBook from '../components/AddBook';
-import Hero from '../components/Hero';
+import '../assets/styles/containers/Shelf.scss';
 
 class Shelf extends React.Component {
   render() {
