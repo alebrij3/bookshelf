84bf335f3b89092289d1cf2e9eefc744890efcfc
Alex <ajf@fastmail.com>
2020-05-16 01:08:51 -0500

    ADD select to filter books by status

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,12 +7,14 @@ class Search extends React.Component {
       <section id="search">
         <form action="">
           <label htmlFor="search-input">Search book title</label>
-          <input id="search-input" type="text" onChange={this.props.handleChange}/>
-          {/* <select name="status" id="">
-            <option value="wantToRead">Want to Read</option>
-            <option value="nowReading">Now Reading</option>
+          <input id="search-input" name="search-query" type="text" onChange={this.props.handleChange}/>
+          <label htmlFor="book-status">Filter by status</label>
+          <select name="status" id="book-status" onChange={this.props.handleChange}>
+            <option value="all">All books</option>
+            <option value="want-to-read">Want to Read</option>
+            <option value="now-reading">Now Reading</option>
             <option value="read">Read</option>
-          </select> */}
+          </select>
         </form>
       </section>
     );
